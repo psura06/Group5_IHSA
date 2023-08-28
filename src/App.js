963504626f386ba6cb1412cfa763d35d46222fc2
Shiva Vangari <49754508+shiva6427@users.noreply.github.com>
2023-08-28 02:04:53 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -27,23 +27,33 @@ const App = () => {
               <Route path="/" element={<HomePage userRole={userRole} handleLogout={handleLogout} />} />
               <Route path="/about" element={<AboutPage userRole={userRole} handleLogout={handleLogout} />} />
               <Route path="/contact" element={<ContactPage userRole={userRole} handleLogout={handleLogout} />} />
-              {userRole === 'admin' && <Route path="/admin" element={<AdminPage userRole={userRole} handleLogout={handleLogout} />} />}
+              {(userRole === 'admin' || userRole === 'showadmin') && (
+                <Route path="/admin" element={<AdminPage userRole={userRole} handleLogout={handleLogout} />} />
+              )}
               <Route
                 path="/login"
                 element={<LoginAdminPage setUserRole={setUserRole} />}
               />
               <Route path="/map" element={<MapPage userRole={userRole} handleLogout={handleLogout} />} />
-              {userRole === 'admin' || userRole === 'showAdmin' ? (
+              {(userRole === 'admin' || userRole === 'showadmin') && (
                 <Route path="/randomize" element={<RandomizePage userRole={userRole} handleLogout={handleLogout} />} />
-              ) : null}
+              )}
               <Route path="/gallery" element={<GalleryPage userRole={userRole} handleLogout={handleLogout} />} />
               <Route path="/announcements" element={<AnnouncementsPage userRole={userRole} handleLogout={handleLogout} />} />
               <Route path="/announcements/:id" element={<AnnouncementsPage userRole={userRole} handleLogout={handleLogout} />} />
               <Route path="/events/:id" element={<EventPage userRole={userRole} handleLogout={handleLogout} />} />
-              {userRole === 'admin' && <Route path="/manage-events" element={<ManageEventsPage userRole={userRole} handleLogout={handleLogout} />} />}
-              {userRole === 'admin' && <Route path="/manage-riders" element={<ManageRidersPage userRole={userRole} handleLogout={handleLogout} />} />}
-              {userRole === 'admin' && <Route path="/manage-horses" element={<ManageHorsesPage userRole={userRole} handleLogout={handleLogout} />} />}
-              {userRole === 'admin' && <Route path="/user-management" element={<UserManagementPage userRole={userRole} handleLogout={handleLogout} />} />}
+              {(userRole === 'admin' || userRole === 'showadmin') && (
+                <Route path="/manage-events" element={<ManageEventsPage userRole={userRole} handleLogout={handleLogout} />} />
+              )}
+              {(userRole === 'admin' || userRole === 'showadmin') && (
+                <Route path="/manage-riders" element={<ManageRidersPage userRole={userRole} handleLogout={handleLogout} />} />
+              )}
+              {(userRole === 'admin' || userRole === 'showadmin') && (
+                <Route path="/manage-horses" element={<ManageHorsesPage userRole={userRole} handleLogout={handleLogout} />} />
+              )}
+              {(userRole === 'admin') && (
+                <Route path="/user-management" element={<UserManagementPage userRole={userRole} handleLogout={handleLogout} />} />
+              )}
             </Routes>
           )}
         </UserHandler>
