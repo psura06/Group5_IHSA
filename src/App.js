e254044c07aed4c3075338e3de99fbce42e74d60
Ambica Peruru <ambicaperuru@gmail.com>
2023-10-18 18:07:13 -0500

    Updated Add.js
    Added a new role super admin and enabled the user management page to edit the passwords.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import ManageEventsPage from './components/ManageEventsPage';
 import ManageRidersPage from './components/ManageRidersPage';
 import ManageHorsesPage from './components/ManageHorsesPage';
 import UserManagementPage from './components/UserManagementPage';
+import ManageAnnouncementsPage from './components/ManageAnnouncementsPage';
 import UserHandler from './UserHandler';
 
 const App = () => {
@@ -27,7 +28,7 @@ const App = () => {
               <Route path="/" element={<HomePage userRole={userRole} handleLogout={handleLogout} />} />
               <Route path="/about" element={<AboutPage userRole={userRole} handleLogout={handleLogout} />} />
               <Route path="/contact" element={<ContactPage userRole={userRole} handleLogout={handleLogout} />} />
-              {(userRole === 'admin' || userRole === 'showadmin') && (
+              {(userRole === 'admin' || userRole === 'showadmin' || userRole === 'superadmin') && (
                 <Route path="/admin" element={<AdminPage userRole={userRole} handleLogout={handleLogout} />} />
               )}
               <Route
@@ -35,23 +36,26 @@ const App = () => {
                 element={<LoginAdminPage setUserRole={setUserRole} />}
               />
               <Route path="/map" element={<MapPage userRole={userRole} handleLogout={handleLogout} />} />
-              {(userRole === 'admin' || userRole === 'showadmin') && (
+              {(userRole === 'admin' || userRole === 'showadmin' || userRole === 'superadmin') && (
                 <Route path="/randomize" element={<RandomizePage userRole={userRole} handleLogout={handleLogout} />} />
               )}
               <Route path="/gallery" element={<GalleryPage userRole={userRole} handleLogout={handleLogout} />} />
               <Route path="/announcements" element={<AnnouncementsPage userRole={userRole} handleLogout={handleLogout} />} />
               <Route path="/announcements/:id" element={<AnnouncementsPage userRole={userRole} handleLogout={handleLogout} />} />
               <Route path="/events/:id" element={<EventPage userRole={userRole} handleLogout={handleLogout} />} />
-              {(userRole === 'admin' || userRole === 'showadmin') && (
+              {(userRole === 'admin' || userRole === 'showadmin' || userRole === 'superadmin') && (
                 <Route path="/manage-events" element={<ManageEventsPage userRole={userRole} handleLogout={handleLogout} />} />
               )}
-              {(userRole === 'admin' || userRole === 'showadmin') && (
+              {(userRole === 'admin' || userRole === 'showadmin' || userRole === 'superadmin') && (
                 <Route path="/manage-riders" element={<ManageRidersPage userRole={userRole} handleLogout={handleLogout} />} />
               )}
-              {(userRole === 'admin' || userRole === 'showadmin') && (
+              {(userRole === 'admin' || userRole === 'showadmin' || userRole === 'superadmin') && (
                 <Route path="/manage-horses" element={<ManageHorsesPage userRole={userRole} handleLogout={handleLogout} />} />
               )}
-              {(userRole === 'admin') && (
+              {(userRole === 'admin' || userRole === 'showadmin' || userRole === 'superadmin') && (
+                <Route path="/manage-announcements" element={<ManageAnnouncementsPage userRole={userRole} handleLogout={handleLogout} />} />
+              )}
+              {(userRole === 'admin' || userRole === 'superadmin') && (
                 <Route path="/user-management" element={<UserManagementPage userRole={userRole} handleLogout={handleLogout} />} />
               )}
             </Routes>
