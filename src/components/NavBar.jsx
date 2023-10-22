5819b5a085ab2311ecbad0ea5474df715e806bc3
Shiva Vangari <49754508+shiva6427@users.noreply.github.com>
2023-10-21 22:02:01 -0500

    Increased Font and Icon Size in NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,18 @@ import { RiHomeFill, RiMapPin2Fill, RiImageFill, RiUserFill, RiCalendarTodoFill,
 import { GiHorseHead } from 'react-icons/gi';
 import { FaUserFriends } from 'react-icons/fa';
 import { BsFillChatQuoteFill } from 'react-icons/bs';
-import { BiShuffle } from 'react-icons/bi'; // Importing Randomize Icon
+import { BiShuffle } from 'react-icons/bi';
 import logo from '../assets/ihsalogo.png';
 import '../stylings/navbar.css';
 import { Modal, message } from 'antd';
+
+const iconStyle = {
+  fontSize: '20px',
+};
+
+const linkStyle = {
+  fontSize: '12px',
+};
 
 const NavBar = ({ userRole, handleLogout }) => {
   const isAdmin = userRole === 'admin';
@@ -37,79 +45,79 @@ const NavBar = ({ userRole, handleLogout }) => {
       </Link>
       <ul className="menu">
         <li>
-          <Link to="/">
-            <RiHomeFill size={20} /> Home
+          <Link to="/" style={linkStyle}>
+            <RiHomeFill style={iconStyle} /> Home
           </Link>
         </li>
         <li>
-          <Link to="/map">
-            <RiMapPin2Fill size={20} /> Map
+          <Link to="/map" style={linkStyle}>
+            <RiMapPin2Fill style={iconStyle} /> Map
           </Link>
         </li>
         {(isAdmin || isShowAdmin || isSuperAdmin) && (
           <>
             <li>
-              <Link to="/manage-events">
-                <RiCalendarTodoFill size={20} /> Manage Events
+              <Link to="/manage-events" style={linkStyle}>
+                <RiCalendarTodoFill style={iconStyle} /> Manage Events
               </Link>
             </li>
             <li>
-              <Link to="/manage-horses">
-                <GiHorseHead size={20} /> Manage Horses
+              <Link to="/manage-horses" style={linkStyle}>
+                <GiHorseHead style={iconStyle} /> Manage Horses
               </Link>
             </li>
             <li>
-              <Link to="/manage-riders">
-                <FaUserFriends size={20} /> Manage Riders
+              <Link to="/manage-riders" style={linkStyle}>
+                <FaUserFriends style={iconStyle} /> Manage Riders
               </Link>
             </li>
             <li>
-              <Link to="/manage-announcements">
-                <BsFillChatQuoteFill size={20} /> Manage Announcements
+              <Link to="/manage-announcements" style={linkStyle}>
+                <BsFillChatQuoteFill style={iconStyle} /> Manage Announcements
               </Link>
             </li>
             {(isAdmin || isSuperAdmin) && (
               <li>
-                <Link to="/user-management">
-                  <RiUserSearchFill size={20} /> User Management
+                <Link to="/user-management" style={linkStyle}>
+                  <RiUserSearchFill style={iconStyle} /> User Management
                 </Link>
               </li>
             )}
             <li>
-              <Link to="/randomize">
-                <BiShuffle size={20} /> Randomize
+              <Link to="/randomize" style={linkStyle}>
+                <BiShuffle style={iconStyle} /> Randomize
               </Link>
             </li>
           </>
         )}
         <li>
-          <Link to="/gallery">
-            <RiImageFill size={20} /> Gallery
+          <Link to="/gallery" style={linkStyle}>
+            <RiImageFill style={iconStyle} /> Gallery
           </Link>
         </li>
         <li>
-          <Link to="/announcements">
-            <BsFillChatQuoteFill size={20} /> Announcements
+          <Link to="/announcements" style={linkStyle}>
+            <BsFillChatQuoteFill style={iconStyle} /> Announcements
           </Link>
         </li>
         <li>
-          <Link to="/about">
-            <RiUserFill size={20} /> About Us
+          <Link to="/about" style={linkStyle}>
+            <RiUserFill style={iconStyle} /> About Us
           </Link>
         </li>
         <li>
-          <Link to="/contact">
-            <RiUserFill size={20} /> Contact
+          <Link to="/contact" style={linkStyle}>
+            <RiUserFill style={iconStyle} /> Contact
           </Link>
         </li>
         <li>
           {userRole ? (
-            <button className="login" onClick={showLogoutModal}>
-              <RiLogoutBoxFill size={20} /> Logout
+            <button className="login" onClick={showLogoutModal} style={linkStyle}>
+              <RiLogoutBoxFill style={iconStyle} /> Logout
             </button>
           ) : (
-            <Link to="/login" className="login">
-              <RiUserFill size={20} /> Login
+            <Link to="/login" className="login" style={linkStyle}>
+              <RiUserFill style={iconStyle} /> Login
             </Link>
           )}
         </li>
